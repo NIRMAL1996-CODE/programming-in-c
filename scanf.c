839dfda61399eed16912cb8453389e1d54bc9100
NIRMAL1996-CODE <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-02-19 13:38:59 +0530

    change

---
+++ scanf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ int main()
+    {
+        int a, b;
+        printf("Enter two number");
+        scanf("%d\n%d",&a,&b);//printing more than 2 numbers y
+        
+        return 0;
+
+    }
