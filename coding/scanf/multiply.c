ba50718c61431c5af9e4af2eff58b046f9729c8a
NIRMAL1996-CODE <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-02-20 19:21:58 +0530

    change

--- coding/scanf/multiply.c
+++ coding/scanf/multiply.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 main()
 {
-    int int a, b;
+    int a, b;
     printf("enter two numbers");
-    scanf("%d\n%d"&a,&b);
+    scanf("%d\n%d",&a,&b);
     printf("sum of a %d and b %d is %d",a,b,a*b);
     return 0;
 }
