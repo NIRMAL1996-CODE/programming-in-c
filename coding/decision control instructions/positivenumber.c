f8b2aa84a31e7d91e9b4d64dca6eeeaa705fe06a
NIRMAL1996-CODE <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-02-21 17:55:24 +0530

    new

---
+++ coding/decision control instructions/positivenumber.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+     main()
+    {
+      int a ; 
+      printf("Enter a number");
+      scanf("%d",&a);
+      if(a>=0)
+      {
+        printf("positive");
+      }
+      else
+      {
+        printf("negative");
+      }
+      return 0;
+
+    }
