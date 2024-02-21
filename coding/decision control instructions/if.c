a47b90f677a37a55d13c7bd2084be9b775e44345
NIRMAL1996-CODE <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-02-21 17:41:40 +0530

    new

---
+++ coding/decision control instructions/if.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter marks");
+    scanf("%d",&a);
+    if(a>90)
+    {
+        printf("grade A");
+    }
+    else
+    {
+        printf("grade B");
+    }
+}
