cb6639afe428d713201b45bf7224623f61cbcb12
NIRMAL1996-CODE <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-02-24 14:12:49 +0530

    new

---
+++ coding/pragram questions/toMultiplyFloatingPointNumbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    
+    double product ,a ,b;
+    printf("enter first number\n");
+    scanf("%lf",&a);
+    printf("enter second number\n");
+     scanf("%lf",&b);
+     product = a*b;
+     printf("Product  = %.2lf",product);
+     return 0;
+
+}
