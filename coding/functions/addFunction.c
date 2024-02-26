6d39ead08326d1eccb0158790a70805f7cf9ae60
NIRMAL1996-CODE <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-02-26 14:14:02 +0530

    new

---
+++ coding/functions/addFunction.c
@@ -0,0 +1,22 @@
+//not getting correct output
+
+#include<stdio.h>
+int main()
+{
+    int a, b;  
+    printf("Enter two number");
+    scanf("%d\n%d",a,b);
+    sum = add(a,b);
+    printf("sum is %d",sum);
+    return 0;
+}
+  
+  void add()
+  {
+    int x, y, z;
+    printf("enter two number");
+    scanf("%d\n%d",x,y);
+    z= x+y;  
+    printf("%d",z);
+    return (x+y);  
+  }
