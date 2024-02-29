41e578ef56d658a87fc2b6c19809e00066b65dbf
NIRMAL1996-CODE <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-02-29 15:41:10 +0530

    revers the number

--- coding/array/reverse.c
+++ coding/array/reverse.c
@@ -2,13 +2,15 @@
 int main()
  {
     int rev[] = {1,2,3,4,5};
-
-    int temp=rev[0];
-        rev[0]=rev[1];
-        rev[1]=rev[2];
-        rev[2]=rev[3];
-        rev[3]=rev[4];
+            //   0 1 2 3 4
+            //   4 3 2 1 0 
+    int temp = rev[0]; 
+        rev[0] =rev[4];
+        rev[4] =rev[1];
+        rev[1]= rev[3];
+        rev[3]= rev[4];
         rev[4]=temp;
+        
     printf("Reverse numbers = %d %d %d %d %d",rev[0],rev[1],rev[2],rev[3],rev[4]);
     return 0;
 
