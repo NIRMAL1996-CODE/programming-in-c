90cbedcd255f6115b823fbcd10e6dff87e7656c5
NIRMAL1996-CODE <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-03-03 13:23:56 +0530

    to find the sum of first and last digit of a number

---
+++ coding/pragram questions/sumOfFirstAndLastDigitOfNumber.c
@@ -0,0 +1,21 @@
+//not working
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("enter a number\n");
+    scanf("%d",num);
+    int i;
+    for( i=2; i<=num-1;i++)
+      {
+        if(num%2==0)
+        break;
+      }
+       if(i==num)
+            printf("%d is a prime number",num);
+        else
+            printf(" %d is not a prime number",num);
+        
+        return 0;
+    
+}
