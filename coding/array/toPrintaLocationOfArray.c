d6e8bf66fc94ab3a5875106172dc56a1dc798a57
NIRMAL1996-CODE <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-03-06 20:17:08 +0530

    program to print all the loctions of array

---
+++ coding/array/toPrintaLocationOfArray.c
@@ -0,0 +1,12 @@
+//program to print all the loctions of array
+#include<stdio.h>
+int main()
+{
+    int a[5];
+    int i;
+    for(i=0;i<5;i++)
+    {
+        printf("%u \n",&a[i]);
+    }
+    return 0;
+}
