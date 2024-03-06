aa4ec34466364d2999daa41930ddd1df77d532cc
NIRMAL1996-CODE <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-03-06 20:10:14 +0530

    types of declaration of array in a c program

---
+++ coding/array/typesOfDeclarationOfArray.c
@@ -0,0 +1,12 @@
+//types of declaration of array in a c program
+#include<stdio.h>
+int main()
+{
+    int a[5];
+    int b[5]={1,2,3,4,5};
+    int c[10]={2,5,6};
+    int d[5]={0};
+    int e[]={1,2,3,4,5,6};
+    
+    return 0;
+}
