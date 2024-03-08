5324c0984e4297d002ec3f67ebea407c840df3f0
NIRMAL1996-CODE <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-03-08 20:13:19 +0530

    Write a C program to print all natural numbers from 1 to 100 using a 'for' loop

---
+++ coding/pragram questions/toPrintNaturalNum1to100.c
@@ -0,0 +1,14 @@
+/*Write a C program to print all natural numbers from 1 to 100 using a 'for' loop*/
+
+#include<stdio.h>
+int main()
+{   
+    printf("print natural numbers from 1 to 100.\n");
+    int i;
+    for( i=1;i<=100;i++)
+    {
+       printf(" %d\n",i);
+    }
+   
+    return 0;
+}
