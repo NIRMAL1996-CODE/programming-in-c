2bbfac9ec077411d33eb7539ebd4389ca25d8d96
NIRMAL1996-CODE <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-03-09 15:31:46 +0530

    write a program to find a square of a number

---
+++ coding/pragram questions/tofindasquare.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int x;
+    printf("to find a square enter a number \n");
+    scanf("%d",&x);
+    
+    printf("square = %.1f", sqrt(x));
+    return 0;
+}
