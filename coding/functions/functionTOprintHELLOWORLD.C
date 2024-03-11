af00da808e875528d9dc8b86677edd654f9e36c2
NIRMAL1996-CODE <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-03-11 13:43:41 +0530

    write aprogram to print hello world by function call

---
+++ coding/functions/functionTOprintHELLOWORLD.C
@@ -0,0 +1,13 @@
+//write aprogram to print hello world by function call
+#include<stdio.h>
+void  print();
+int main()
+{
+    print();
+    return 0;
+}
+
+ void print()
+{   
+    printf("HELLO WORLD");
+}
