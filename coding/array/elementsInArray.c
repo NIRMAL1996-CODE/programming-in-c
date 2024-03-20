38b7097d9a582f085a5a5fc2750a959dd891b21c
NIRMAL1996-CODE <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-03-20 20:39:07 +0530

    write a  c program to read n number of elements in an array

--- coding/array/elementsInArray.c
+++ coding/array/elementsInArray.c
@@ -6,7 +6,8 @@ int main()
   int arr[]= {1 ,2 ,3 , 4, 5}, i;
   int size = sizeof(arr)/sizeof(arr[0]);
   printf("number of elements in an array are %d\n", size);
-  for( i=4; i>=0; i--)
+  size-=1;
+  for( i=size; i>=0; i--)
   {
      printf("%d ",arr[i]);
   }
