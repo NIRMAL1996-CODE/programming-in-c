e4c8ac6d54c39030bd02139f2dc33cbd12adf7ce
NIRMAL1996-CODE <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-04-03 12:56:28 +0530

    how would you declare a boolean variable in c

---
+++ coding/boolean programs/declaration.c
@@ -0,0 +1,13 @@
+//how would you declare a boolean variable in c//
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+    bool a=false;
+    bool b= true;
+    bool c= 0;
+    bool d= 1;
+   // bool x=True; //cnt write in upper case
+    printf("%d\n%d\n%d\n%d",a,b,c,d);
+    return 0;
+}
