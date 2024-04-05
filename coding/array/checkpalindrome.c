9f12799e4bc0c64c58fc8615afce7fe581173ccf
NIRMAL1996-CODE <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-04-05 21:01:25 +0530

    write a program to find the given array is palinfrome or not

---
+++ coding/array/checkpalindrome.c
@@ -0,0 +1,29 @@
+//write a program to find the given array is palinfrome or not//
+#include<stdio.h>
+int palindrome(int n[], int size);
+int main()
+{   
+    int result=0 ,size,a[size];
+     size= sizeof(a)/sizeof(a[0]);
+    printf("ENTER %d  elements= ", size);
+    scanf("%d",&a[size]);
+    result= palindrome(a, size);
+    if(result==a[size])
+    {
+    printf(" %d is palindrome number\n",a[size]);
+    }
+    else
+    {
+     printf("%d is not a palindrome number\n",a[size]);
+    }
+    return 0;
+}
+int palindrome(int n[],int size)
+{   
+    int rev=0;
+    for(int i=size;i>0;i--)
+    {
+         rev= n[i];
+        printf("%d",rev);
+    }
+}
