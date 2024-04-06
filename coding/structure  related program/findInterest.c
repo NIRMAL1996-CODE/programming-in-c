44031fda3787643d94c975060f7a5e60f65b3cad
NIRMAL1996-CODE <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-04-06 14:39:05 +0530

    write a program to find the simple interest

--- coding/structure  related program/findInterest.c
+++ coding/structure  related program/findInterest.c
@@ -6,16 +6,22 @@ struct simpleInterest
     float rate;
     int ttime;
 };
+int s_interest(struct simpleInterest si);
 int main()
 {
     struct simpleInterest si;
     printf("enter principle amount= ");
     scanf("%d",&si.principle);
      printf("enter rate of interest= ");
-    scanf("%d",&si.rate);
+    scanf("%f",&si.rate);
      printf("enter time(in years)= ");
     scanf("%d",&si.ttime);
-    float simpleinterest= (si.principle * si.rate * si.ttime)/100;
-    printf("simpleinterest = %.2f",simpleinterest);
+    int interest= s_interest(si);
+    printf("simpleinterest = %d",interest);
     return 0;
+}
+int s_interest(struct simpleInterest si)
+{
+    float s_interest= (si.principle * si.rate * si.ttime)/100;
+    return s_interest;
 }
