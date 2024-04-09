791b4b908a8bd97fd513d7861e936efabcf1b1ca
NIRMAL1996-CODE <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-04-09 20:45:19 +0530

    write a program to chcek if a matrix is symmentric or not

---
+++ coding/matrix/symmetricmatrix.c
@@ -0,0 +1,46 @@
+//write a program to chcek if a matrix is symmentric or not//
+#include<stdio.h>
+int main()
+{
+    int a[2][2], b[2][2],i,j;
+    printf("ENTER first matrix ELEMENTS=\n");
+      for( i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            scanf("%d ",&a[i][j]);
+        }
+    }
+        printf("ELEMENTS=\n");
+      for( i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+    for( i=0;i<2;i++)//transpose
+    {
+        for(j=0;j<2;j++)
+        {
+           b[j][i]=a[i][j];
+        }
+    }    
+    for( i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            printf("%d ",b[i][j]);
+        }
+        printf("\n");
+    }
+    if( a[i][j]==b[i][j])
+    {
+        printf("matrix is symentric");
+    }
+    else
+    printf("matrix is not symentric");
+    
+    return 0;
+}
