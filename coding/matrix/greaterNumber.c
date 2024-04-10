91696ad2d17ff850ce5910ac508802f92ea0e956
NIRMAL1996-CODE <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-04-10 13:03:35 +0530

    write a program to find the greater number of the matrix

---
+++ coding/matrix/greaterNumber.c
@@ -0,0 +1,35 @@
+//write a program to find the greater number of the matrix //
+#include<stdio.h>
+int main()
+{
+    int a[2][2], i, j;
+    int max;
+    int size= sizeof(a)/sizeof(a[0][0]);
+    printf("enter elements \n");
+    for( i=0;i<2;i++)
+    {
+        for( j=0;j<2;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+     for( i=0;i<2;i++)
+    {
+      for( j=0;j<2;j++)
+        {
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+    max=a[0][0];
+     for( i=0;i<2;i++)
+    {
+      for( j=0;j<2;j++)
+        if(max<a[i][j])
+         {
+           max=a[i][j];
+          }
+    }
+    printf("greatest number in a matrix = %d",max);
+   return 0;
+}
