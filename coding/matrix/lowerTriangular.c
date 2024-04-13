d0709d6978e30c1bb943952247e4945e3f738fb7
NIRMAL1996-CODE <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-04-13 13:21:59 +0530

    write a lower triangular of matrix

---
+++ coding/matrix/lowerTriangular.c
@@ -0,0 +1,38 @@
+//write a lower triangular of matrix//
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+    int a[3][3],i,j;
+    bool equal;
+    printf("ENTER matrix ELEMENTS=\n");
+      for( i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+        printf("ELEMENTS=\n");
+      for( i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("LOWER TRIANGULAR OF MATRIX ARE= \n");
+    for( i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+           if(i>j)
+           {
+             printf("%d ",a[i][j]);
+           }
+        }
+          printf("\n");
+    }    
+    return 0;
+}
